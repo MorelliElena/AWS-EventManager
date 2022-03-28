d2ff607797159a7e0818cb992093c4326c01279a
Elena Morelli <elena.morelli3@studio.unibo.it>
2022-03-28 10:29:10 +0200

    change loading spinner display

--- client/src/components/events/EventInfo.js
+++ client/src/components/events/EventInfo.js
@@ -46,83 +46,81 @@ class EventInfo extends React.Component {
     }
 
     render() {
-        if (!this.state.showDefaultMessage && !this.state.eventInfo) {
-            return <Spinner/>
-        } else if(!this.state.showDefaultMessage && this.state.eventInfo) {
-            if(this.state.redirection){
-                return <Redirect to={routes.login}/>
-            }
-            return (
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-3 col-5 px-1 position-fixed" id="sticky-sidebar">
-                            <Navbar state = {false}/>
-                        </div>
-                        <div className="col-md-9 col-7 offset-md-3 offset-5 ps-0 pe-1 pt-0">
-                            <Header/>
-                            <div className="text-center h-100 pt-3">
-                                {this.state.eventInfo.img ?
-                                <img className="card-img-top size"
-                                     src={this.state.eventInfo.img} alt="Event"/> : null}
-                                <div className="card-body p-2">
-                                    <h5 className="card-title">{this.state.eventInfo.name}</h5>
-                                    <section className="card-text">
-                                        <p>{this.state.eventInfo.description}</p>
-                                        <section>
-                                            {this.state.eventInfo.date_start} - {this.state.eventInfo.date_finish}<br/>
-                                            {this.state.eventInfo.location.address} <br/>
-                                            {this.state.eventInfo.location.city} <br/>
-                                            {this.state.eventInfo.location.province}
-                                        </section><br/>
-                                        <section className="mb-4">
-                                            {this.state.eventInfo.tags.map((tag) =>
-                                                <span className="badge rounded-pill bg-info text-dark"
-                                                      key = {"tag" + tag + this.state.eventInfo._id}
-                                                      style={{marginLeft: .65, marginRight: .65}}>
-                                                {tag}
-                                                </span>
-                                            )}
-                                        </section>
-                                        <section>
-                                            {this.state.eventInfo.full ?
-                                                <div style={{color: "red"}}>
-                                                    Posti Terminati
-                                                    <BsFillExclamationCircleFill
-                                                        className="text-danger mx-3" size={26}/>
-                                                </div>:<div/>
-                                            }
-                                        </section>
-                                        <div className="d-flex flex-column justify-content-center">
-                                            <ul className="list-group text-start book">
-                                                { this.state.eventInfo.booking.map(day =>
-                                                    <li className="list-group-item text-center"
-                                                        key = {"booking"+ day.id}>
-                                                        <div>
-                                                            <div>
-                                                                Data: {Api.mapDate(day.date)} <br/>
-                                                                Posti Occupati: {day.n_participants}/
-                                                                {day.max_participants}
-                                                            </div>
-                                                            <div>
-                                                                <Button className="btn btn-primary mt-3 button"
-                                                                        onClick={()=> this.bookInfo(day)}>
-                                                                    Prenota
-                                                                </Button>
-                                                            </div>
-                                                        </div>
-                                                    </li>
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-3 col-5 px-1 position-fixed" id="sticky-sidebar">
+                        <Navbar state = {false}/>
+                    </div>
+                    <div className="col-md-9 col-7 offset-md-3 offset-5 ps-0 pe-1 pt-0">
+                        <Header/>
+                        { !this.state.showDefaultMessage && !this.state.eventInfo ?
+                            <div className="text-center h-100">
+                                <Spinner/>
+                            </div> :
+                            this.state.redirection ? <Redirect to={routes.login}/> :
+                                <div className="text-center h-100 pt-3">
+                                    {this.state.eventInfo.img ?
+                                        <img className="card-img-top size"
+                                             src={this.state.eventInfo.img} alt="Event"/> : null}
+                                    <div className="card-body p-2">
+                                        <h5 className="card-title">{this.state.eventInfo.name}</h5>
+                                        <section className="card-text">
+                                            <p>{this.state.eventInfo.description}</p>
+                                            <section>
+                                                {this.state.eventInfo.date_start} - {this.state.eventInfo.date_finish}<br/>
+                                                {this.state.eventInfo.location.address} <br/>
+                                                {this.state.eventInfo.location.city} <br/>
+                                                {this.state.eventInfo.location.province}
+                                            </section><br/>
+                                            <section className="mb-4">
+                                                {this.state.eventInfo.tags.map((tag) =>
+                                                        <span className="badge rounded-pill bg-info text-dark"
+                                                              key = {"tag" + tag + this.state.eventInfo._id}
+                                                              style={{marginLeft: .65, marginRight: .65}}>
+                                                            {tag}
+                                                        </span>
                                                 )}
-                                            </ul>
-                                        </div>
+                                            </section>
+                                            <section>
+                                                {this.state.eventInfo.full ?
+                                                    <div style={{color: "red"}}>
+                                                        Posti Terminati
+                                                        <BsFillExclamationCircleFill
+                                                            className="text-danger mx-3" size={26}/>
+                                                    </div>:<div/>
+                                                }
+                                            </section>
+                                            <div className="d-flex flex-column justify-content-center">
+                                                <ul className="list-group text-start book">
+                                                    { this.state.eventInfo.booking.map(day =>
+                                                        <li className="list-group-item text-center"
+                                                            key = {"booking"+ day.id}>
+                                                            <div>
+                                                                <div>
+                                                                    Data: {Api.mapDate(day.date)} <br/>
+                                                                    Posti Occupati: {day.n_participants}/
+                                                                    {day.max_participants}
+                                                                </div>
+                                                                <div>
+                                                                    <Button className="btn btn-primary mt-3 button"
+                                                                            onClick={()=> this.bookInfo(day)}>
+                                                                        Prenota
+                                                                    </Button>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    )}
+                                                </ul>
+                                            </div>
                                     </section>
                                 </div>
                             </div>
-                        </div>
+                        }
                     </div>
                 </div>
-            );
-        }
-
+            </div>
+        );
     }
 }
 export default EventInfo
